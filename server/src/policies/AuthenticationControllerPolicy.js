c4516b5ae5e6ad13372f10e569373a79c31c5782
MitchellThomson <mitchell.thomson@code.berlin>
2022-05-11 12:01:38 +0200

    Added joi policy

---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,38 @@
+const Joi = require('joi')
+
+module.exports = {
+    register (req, res, next) {
+        const schema = Joi.object({
+            email: Joi.string().email(),
+            password: Joi.string().regex(
+                new RegExp('^[a-zA-Z0-9]{8,32}$')
+            )
+        })
+
+        const {error, value} = schema.validate(req.body)
+
+        if(error){
+            switch(error.details[0].context.key){
+            case'email':
+                res.status(400).send({
+                    error:'Please provide a valid email address'
+                })
+                break
+            case 'password':
+                res.status(400).send({
+                    error: `Your password must follow these rules: <br>
+                    1: Only a-z A-Z 0-9 are allowed as valid characters <br>
+                    2: Minimum length of 8 characters <br>
+                    3: Maximum length of 32 characters`
+                })
+                break
+            default:
+                res.status(400).send({
+                    error: 'Registration info is invalid'
+                })
+            }
+        }else{
+            next()
+        }
+    }
+}
